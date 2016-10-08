1628be3d95d16342d270f8a9aabddb0e6ce0b968
Mridul-Sagar <msagar@expedia.com>
2016-10-08 18:36:04 +0530

    D3 primitives

--- script.js
+++ script.js
@@ -192,3 +192,25 @@
 
 //---------SVG----------//  
 
+// d3.select('#chart')
+//   .append('svg')
+//   	.attr('width', 600)
+//   	.attr('height', 400)
+//   	.style('background', "#93A1A1")
+//   .append("rect")
+//   	.attr('x', 200)
+//   	.attr('y', 100)
+//   	.attr('height', 200)
+//   	.attr('widtch', 200)
+//   	.style('fill', '#CB4B19')	
+//   d3.select('svg')
+// 	  .append('circle')
+// 	  	.attr('cx', '300')
+// 	  	.attr('cy', '200')
+// 	  	.attr('r', '50')
+// 	  	.style('fill', '#840043')    //second d3.select is part of the first; hence, the whole statement is a single command
+
+
+
+//---------SVG BarChart----------//   
+
